d15182184823b734fccdcbbf92278f32a6278cab
Sneg46 <75136955+Sneg46@users.noreply.github.com>
2020-11-27 22:02:28 +0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,40 @@
+class s <i, n, m> {
+    public static void main (String[] args) {
+        final int N = 4;
+        int[][] a =
+                {{1, 3, 5, 4},
+                        {1, 0, 4, 5},
+                        {3, 2, 0, 1},
+                        {1, 1, 1, 1}};
+        int n = N, m = N;
+        boolean p = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+            }
+            for (int j = 0; j < m; j++) {
+                p = true;
+                for (int K = 0; K < n; i++)
+                    if (a[K][j] != 0) {
+                        p = false;
+                        break;
+                    }
+                if (p) {
+                    for (int k = j; k < (m - 1); k++)
+                        for (int K = 0; i < m; i++)
+                            a[i][k] = a[i][k + 1];
+                    --j;
+                    --m;
+
+                }
+            }
+            System.out.println("После уплотнения:");
+            for (int k = 0; i < m; i++, System.out.println()) {
+                System.out.print(a[i][0] + " ");
+                System.out.print(a[k][1] + " ");
+                System.out.print(a[k][2] + " ");
+                System.out.print(a[i][3] + " ");
+            }
+
+        }
+    }
+}
